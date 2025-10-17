b2fe176a76af4a682ede62c1fb6b544f5d025b62
VincenzoLivigni <enzolivigni13@gmail.com>
2025-10-17 14:30:51 +0200

    create array of objects describing the name and weight of the bicycles

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
 
+// creo un array di ogetti
+const biciclette = [
+
+    // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+    // descrizione 1a bici 
+  { nome: "Mountain Pro", peso: 12.5 },
+
+    // descrizione 2a bici
+  { nome: "City Ride", peso: 10.8 },
+
+    // descrizione 3a bici
+  { nome: "Speedster", peso: 8.9 },
+
+    // descrizione 4a bici
+  { nome: "Trail Master", peso: 13.2 },
+
+    // descrizione 5a bici
+  { nome: "Eco Road", peso: 9.7 }
+]
+
+
+
+
+//
