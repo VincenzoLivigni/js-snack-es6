19496641c1ed9e33c406cf9709b70e54e0952e0a
VincenzoLivigni <enzolivigni13@gmail.com>
2025-10-17 16:20:57 +0200

    generate random numbers for puntiFatti and falliSubiti

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const biciclette = [
 console.log(biciclette);
 
 // variabile di supporto
-let pesoMinore = biciclette[0];
+let pesoMinore = biciclette[1];
 
     // ciclo per trovare il peso di tutte le biciclette
     for (let i = 0; i < biciclette.length; i++) {
@@ -65,10 +65,25 @@ const squadre = [
   { nome: "Roma", puntiFatti: 0, falliSubiti: 0 },
   { nome: "Lazio", puntiFatti: 0, falliSubiti: 0 }
 ];
+// console.log(squadre);
 
-console.log(squadre);
+// ciclo nell'array per prendere gli oggetti singolarmente
+for (let i = 0; i < squadre.length; i++) {
+    const statistiche = squadre[i];
+    // console.log(statistiche);
+    // console.log(statistiche.puntiFatti);
+
+    // se ho "0" nelle nelle proprietà “punti” fatti e “falli subiti”.
+    if (statistiche.puntiFatti === 0 && statistiche.falliSubiti=== 0){
+        // genero randomicamente dei numeri per puntiFatti e falliSubiti
+        statistiche.puntiFatti = Math.floor(Math.random() *98) +1;
+        statistiche.falliSubiti = Math.floor(Math.random() *98) +1;
+    }
+}
+console.log(squadre)
 
 
-// genero randomicamente dei numeri 
-// - se ho "0" nelle nelle proprietà “punti” fatti e “falli subiti”.
-
+/*
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
